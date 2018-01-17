525b07f779346aa05a7588acd1c5e0626a0f3e31
elish <egoyani@yahoo.com>
2018-01-17 14:25:11 +0530

    latest1

--- EntityFrameWorkDemo/Models/Student.cs
+++ EntityFrameWorkDemo/Models/Student.cs
@@ -12,6 +12,6 @@ namespace EntityFrameWorkDemo.Models
         public string StudentName { get; set; }
         public string StudentClass { get; set; }
         public string City { get; set; }
-       
+
     }
 }
